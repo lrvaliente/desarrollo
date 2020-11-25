d9bee468095d76d8564561f02f51498f1e2d5c77
Lucio Rodriguez <luciorodriguez@MacBook-Air-de-Lucio.local>
2020-11-25 13:22:32 -0300

    Actualización 7

--- test/src/test/java/py/mi/test/HelloWorldTest.java
+++ test/src/test/java/py/mi/test/HelloWorldTest.java
@@ -9,15 +9,19 @@ import org.junit.jupiter.api.DisplayName;
 @DisplayName("Test form HelloWorldTest")
 public class HelloWorldTest {
 
-	private static final String VALOR = "Home";
+	private static final String VALOR = "Index";
 	
 	private HelloWorld h = new HelloWorld();
 
 	public HelloWorldTest() {}
 
 	@Test
-	public void test() {		
+	public void test1() {		
 		assertEquals(VALOR, h.verificar());
 	}
 
+	@Test
+	public void test2() {
+		assertEquals("Home", h.verificar());
+	}
 }
